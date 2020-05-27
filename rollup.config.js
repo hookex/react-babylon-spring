cfa738d5bed6cc2fe810d5166edf6c40d8ff4c2e
hookex <cookmycode@gmail.com>
2020-05-27 17:38:56 +0800

    feat: rollup build

--- rollup.config.js
+++ rollup.config.js
@@ -7,22 +7,13 @@ const libraryName = pkg.name
 const isProduction = process.env.NODE_ENV === 'production';
 console.log('ENV:', process.env.NODE_ENV, libraryName)
 
-const exportGlobals = {
-    'react': 'React',
-    'react-dom': 'ReactDom',
-    'react-reconciler': 'ReactReconciler',
-    '@babylonjs/core': 'BabylonjsCore'
-}
-
 export default (async () => {
-
     const result = {
-        input: `src/${libraryName}.ts`,
+        input: `src/index.ts`,
         output: [{
             file: pkg.module,
             format: 'es',
             sourcemap: true,
-            globals: exportGlobals,
         }],
         context: 'window',
         // external modules not in bundle (i.e.: 'react')
