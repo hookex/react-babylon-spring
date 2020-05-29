36b3cd056d143fe3ed58af8a7c6e0390060c0f83
hookex <cookmycode@gmail.com>
2020-05-29 11:30:15 +0800

    feat: story book done

---
+++ .storybook/main.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+    stories: ['../stories/**/*.stories.[tj]s'],
+    addons: [
+        '@storybook/addon-actions/register',
+        '@storybook/addon-links/register',
+        '@storybook/addon-storysource/register',
+        '@storybook/addon-notes/register',
+    ],
+    webpackFinal: async (config, { configType }) => {
+        config.module.rules.push({
+            test: /\.stories\.jsx?$/,
+            loaders: [require.resolve('@storybook/source-loader')],
+            enforce: 'pre',
+          });
+        console.log(`added source loader to '${configType}' webpack config`);
+        // Return the altered config
+        return config;
+    },
+};
